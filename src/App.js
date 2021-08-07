92ad1edaaf49f231e6e08754336c12f13ebf25ef
Amr Mohamed <amr.mohamed5053@outlook.com>
2021-08-07 10:45:48 +0200

    last pushing change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 import Profile from './Profile';
-function App() {
-  return (
-    <Profile/>
-    );
+import React, { Component } from 'react';
+class APP extends Component {
+  state = {  
+      show: false,
+      count:0,
+  }
+  myinterval = null;
+  
+reset = () => {
+  this.setState((prev) => ({...prev , count : 0}))
+  clearInterval(this.myinterval)
+  this.myinterval = setInterval(() => {
+    this.setState((prev) => ({...prev, count: prev.count + 1}))
+  }, 1000);
 }
-
-export default App;
+  render() { 
+    return (  
+      <>
+      {this.state.show && <Profile reset={this.reset} />}
+      <button className='btn btn-primary' onClick={() => this.setState((prev) => ({...prev, show: !prev.show}))}>{this.state.show === true ? 'Hide': 'Show'}</button>
+      <p>The component has been rendered for {this.state.count} seconds</p>
+      </>
+    );
+  }
+}
+ 
+export default APP;
--- src/Profile.js
+++ src/Profile.js
@@ -1,45 +1,26 @@
 import React, { Component } from 'react';
 import myImage from './amr.jpg'
 class Profile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            fullName:'Amr Mohamed',
-            bio: 'Web Developer ',
-            profession: 'Frontend Developer',
-            show: true,
-            count:0,
-         }
-    }
-    toggle = () => {
-        this.setState({
-            show: !this.state.show
-        })  
+    state = {
+        fullName:'Amr Mohamed',
+      bio: 'Web Developer ',
+      profession: 'Frontend Developer',
     }
     componentDidMount () {
-        this.myInterval = setInterval (() => {
-            this.setState({
-                count: this.state.count + 1 
-            })
-        },1000)
-}
-    componentWillUnmount () {
-        clearInterval(this.myInterval)
+        this.props.reset()
     }
+
     render() { 
         return ( 
             <div>
-                    {this.state.show &&
                 <div>
                 <img src={myImage} alt ='myImage' />
                 <h1>{this.state.fullName}</h1>
                 <h3>{this.state.bio}</h3>
                 <h5>{this.state.profession}</h5>
-                <p>The component has been rendered for {this.state.count} seconds</p>
-                </div>}
-                <button className='btn btn-primary' onClick={this.toggle}>{this.state.show === true ? 'Hide': 'Show'}</button>
+                </div>
             </div>
-         );
+         )
     }
 }
  
