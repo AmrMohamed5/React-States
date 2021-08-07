fc78c45da152cdce0d5dd2e1e308a59132c8815f
Amr Mohamed <amr.mohamed5053@outlook.com>
2021-08-07 03:01:17 +0200

    last modify

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import myImage from './amr.jpg'
 class Profile extends Component {
     constructor(props) {
         super(props);
@@ -30,12 +31,13 @@ class Profile extends Component {
             <div>
                     {this.state.show &&
                 <div>
+                <img src={myImage} alt ='myImage' />
                 <h1>{this.state.fullName}</h1>
                 <h3>{this.state.bio}</h3>
                 <h5>{this.state.profession}</h5>
                 <p>The component has been rendered for {this.state.count} seconds</p>
                 </div>}
-                <button onClick={this.toggle}>{this.state.show === true ? 'Hide': 'Show'}</button>
+                <button className='btn btn-primary' onClick={this.toggle}>{this.state.show === true ? 'Hide': 'Show'}</button>
             </div>
          );
     }
